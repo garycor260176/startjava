1905be2c4e7fc264f6d65262543507fc5bf91a32
garycor260176 <ipolnikov@gmail.com>
2022-11-10 15:38:24 +0300

    commit L_01_01

--- L_01_01.java
+++ L_01_01.java
@@ -1,28 +1,28 @@
 public class L_01_01 {    
 
     public static void main(String[] args) {
-        char c_symbol = 'a';
-        boolean b_any_exist = true;
+        char symbol = 'a';
+        boolean any_exist = true;
         int availableProcessors = Runtime.getRuntime().availableProcessors();
         byte byte_availableProcessors = (byte)availableProcessors;
         short short_availableProcessors = (short)availableProcessors;
-        long v_totalMemory = Runtime.getRuntime().totalMemory();
-        long v_freeMemory = Runtime.getRuntime().freeMemory();
-        long v_maxMemory = Runtime.getRuntime().maxMemory();
-        String s_ArchName = System.getProperties().getProperty ("os.arch");
-        float f_cpuFreq = 4.6f; //как найти частоту не используя стороннние библиотеки не нашел. и сдается мне работа от ОС будет зависеть
-        double d_cpuFreq = f_cpuFreq;
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        String ArchName = System.getProperties().getProperty ("os.arch");
+        float cpuFreq = 4.6f; //как найти частоту не используя стороннние библиотеки не нашел. и сдается мне работа от ОС будет зависеть
+        double double_cpuFreq = cpuFreq;
 
         System.out.println("(int)availableProcessors = " + availableProcessors );
         System.out.println("(byte)availableProcessors = " + byte_availableProcessors );
         System.out.println("(short)availableProcessors = " + short_availableProcessors );
-        System.out.println("totalMemory = " + v_totalMemory);
-        System.out.println("freeMemory = " + v_freeMemory);
-        System.out.println("maxMemory = " + v_maxMemory);
-        System.out.println("Архитектура операционной системы: " + s_ArchName);
-        System.out.println("(float)Частота процессора: " + f_cpuFreq + " ГГц");
-        System.out.println("(double)Частота процессора: " + d_cpuFreq + " ГГц");
-        System.out.println("Что-то наличиствует? " + b_any_exist);
-        System.out.println("Просто буква: '" + c_symbol + "'");
+        System.out.println("totalMemory = " + totalMemory);
+        System.out.println("freeMemory = " + freeMemory);
+        System.out.println("maxMemory = " + maxMemory);
+        System.out.println("Архитектура операционной системы: " + ArchName);
+        System.out.println("(float)Частота процессора: " + cpuFreq + " ГГц");
+        System.out.println("(double)Частота процессора: " + double_cpuFreq + " ГГц");
+        System.out.println("Что-то наличиствует? " + any_exist);
+        System.out.println("Просто буква: '" + symbol + "'");
     }
 }
